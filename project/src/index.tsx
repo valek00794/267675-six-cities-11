8e2f34608d6c4438a1269b58082104137d31649b
Valentin <valentin_00794@bk.ru>
2022-10-23 11:47:08 +0300

    Добавит компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.querySelector('#root') as HTMLElement,
 );
 
 root.render(
