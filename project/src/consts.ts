edaae6733fec777fea113dee20d73ab104696e2d
Valentin <valentin_00794@bk.ru>
2022-10-27 00:14:08 +0300

    Добавит компонент приватного маршрута

--- project/src/consts.ts
+++ project/src/consts.ts
@@ -4,3 +4,10 @@ export enum AppRoute {
     Favorites = '/favorites',
     Room = '/offer/:id',
 }
+
+
+export enum AuthorizationStatus {
+    Auth = 'AUTH',
+    NoAuth = 'NO_AUTH',
+    Unknown = 'UNKNOWN',
+  }
