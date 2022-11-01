9803af15243b46d3cdf5052dd63b3787a05419e3
Valentin <valentin_00794@bk.ru>
2022-11-01 15:52:47 +0300

    Добавляет компоненты
    -компоненты review, room-review
    -моки для комментариев
    -частично страница о комнате

--- project/src/types/offers.ts
+++ project/src/types/offers.ts
@@ -1,7 +1,7 @@
 export type Offer = {
   bedrooms: number;
   description: string;
-  goods: [string];
+  goods: string[];
   host: {
    avatarUrl: string;
    id: number;
@@ -9,7 +9,7 @@ export type Offer = {
    name: string;
   };
   id: number;
-  images: [string];
+  images: string[];
   isFavorite: boolean;
   isPremium: boolean;
   location: {
