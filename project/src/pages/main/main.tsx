90ee576d93e666eb02ed3d55e2ac053564b8d804
Valentin <valentin_00794@bk.ru>
2022-11-07 15:39:55 +0300

    Фикс шапки
    - шавка в роутаз вместо каждой странице

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 import {useRef, useState} from 'react';
 
-import Header from '../../components/header/header';
+
 import CitiesList from '../../components/cities-list/cities-list';
 import Sort from '../../components/sort/sort';
 import OfferList from '../../components/offers-list/offers-list';
@@ -17,7 +18,6 @@ function Main(): JSX.Element {
   const sortUlState = useState(false);
   return (
     <div className="page page--gray page--main">
-      <Header />
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <CitiesList sortRef={sortRef} sortUlState={sortUlState} />
