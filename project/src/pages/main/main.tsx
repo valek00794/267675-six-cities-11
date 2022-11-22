c314f47b71ffb176c66701d38281101475373c5f
Valentin <valentin_00794@bk.ru>
2022-11-22 21:53:10 +0300

    Компонент main-empty

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,10 +1,12 @@
 import {useRef, useState} from 'react';
 import {useParams} from 'react-router';
+import classnames from 'classnames';
 
 import CitiesList from '../../components/cities-list/cities-list';
 import Sort from '../../components/sort/sort';
 import OffersList from '../../components/offers-list/offers-list';
 import Map from '../../components/map/map';
+import MainEmpty from '../../components/main-empty/main-empty';
 import NotFound from '../../pages/not-found/not-found';
 
 import {useAppSelector} from '../../hooks';
@@ -20,35 +22,40 @@ function Main(): JSX.Element {
   const {city} = useParams();
   const offers = useAppSelector((state) => getSortOffers(state, city, sortRef.current));
 
+
   if (city && !cities.includes(city)) {
     return <NotFound />;
   }
 
   return (
     <div className="page page--gray page--main">
-      <main className="page__main page__main--index">
+      <main className={classnames(
+        'page__main page__main--index',
+        {'page__main--index-empty': offers.length === 0}
+      )}
+      >
         <h1 className="visually-hidden">Cities</h1>
         <CitiesList sortRef={sortRef} setUlState={setUlState} />
         <div className="cities">
+          {offers.length === 0 && <MainEmpty />}
+          {offers.length !== 0 &&
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offers.length !== 0 && city ? `${offers.length} places to stay in ${city}` : 'No places to stay available'} </b>
-              {offers.length !== 0 &&
-              <Sort sort={sortRef.current} sortRef={sortRef} sortUlState={sortUlState} setUlState={setUlState}/>}
+              <Sort sort={sortRef.current} sortRef={sortRef} sortUlState={sortUlState} setUlState={setUlState}/>
               <OffersList sort={sortRef.current} setActiveCard={setActiveCard} />
             </section>
             <div className="cities__right-section">
               <section className="cities__map">
-                {offers.length !== 0 &&
                 <Map
                   offers={offers}
                   selectedCard={selectedCard}
                   mapStyle={MapStyle.Main}
-                />}
+                />
               </section>
             </div>
-          </div>
+          </div>}
         </div>
       </main>
     </div>
