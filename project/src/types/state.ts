9b44de66fb0d7fdfb72438949a38ec8931d83ed2
Valentin <valentin_00794@bk.ru>
2022-11-23 01:19:56 +0300

    Функционал добавления в избранное WIP
    WIP
    -не обновляется кнопка на главной странице
    -не считает количество в избранном

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -1,18 +1,19 @@
 import {store} from '../store/index';
 import {AuthorizationStatus} from '../consts';
-import { Offer } from './offers';
-import { Comment } from './comment';
+import {Offer} from './offers';
+import {Comment} from './comment';
 
 export type AppData = {
-  serverOffers: Offer[];
   offers: Offer[];
-  serverOffer: Offer;
-  serverComments: Comment[];
-  serverNearbyOffers: Offer[];
+  offer: Offer;
+  comments: Comment[];
+  nearbyOffers: Offer[];
+  favoriteOffers: Offer[];
   isOffersDataLoading: boolean;
   isOfferDataLoading: boolean;
   isCommentsDataLoading: boolean;
   isNearbyOffersDataLoading: boolean;
+  isFavoriteOffersDataLoading: boolean;
   isCommentPostStatus: boolean;
   isCommentSubmitSuccessful: boolean;
 };
